ff667a16fed9e2bbc3fae5302b10cc2d028d9782
Chanwoo1124 <lgw22774@gmail.com>
2025-12-17 22:14:06 +0900

    Laravel

---
+++ intro_example/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $faker = fake();
+
+    $chatMessages = $faker->sentences($faker->numberBetween(4,10));
+
+    return view('welcome', ['chatMessages'=>$chatMessages]);
+
+});
+
+Route::get('/dashboard', function () {
+    $faker = fake();
+
+    return view('dashboard',[
+        'welcomeMessages' => $faker -> paragraphs(5),
+    ]);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
